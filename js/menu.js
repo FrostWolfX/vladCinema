8ae3df74e702032055e0f922843b15c578c2bec9
Vlad <stukalov.blog@gmail.com>
2021-12-19 22:24:33 +0300

    first commit

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+const slideMenu = (openBtn, menu, classActiveMenu, closeTrigger) => {
+    const burgerBtn = document.querySelector(openBtn);
+    const navigation = document.querySelector(menu);
+    console.log(burgerBtn)
+};
+
+export default slideMenu;
