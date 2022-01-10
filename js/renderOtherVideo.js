f270e0f3e30f666f93ea69dfee11985766a060ec
Vlad <stukalov.blog@gmail.com>
2022-01-11 00:42:10 +0300

    add other films

---
+++ js/renderOtherVideo.js
@@ -0,0 +1,27 @@
+import { getTriends } from "./services.js";
+
+const otherFilmsList = document.querySelector('.other-films__list');
+
+const otherFilmsHTML = data => {
+    let tempData = '';
+    console.log(data);
+    data.forEach(element => {
+        tempData += `
+            <li class="other-films__item">
+                <a class="other-films__link" data-rating="${element.vote_average}">
+                    <img class="other-films__img" 
+                        src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2${element.poster_path}" 
+                        alt="постер">
+                </a>
+            </li>
+        `;
+    });
+    otherFilmsList.innerHTML = tempData; 
+};
+
+const renderVideo = async () => {
+    const data = await getTriends();
+    otherFilmsHTML(data.results)
+};
+
+export default renderVideo;
