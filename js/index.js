863487ed3808a402d6a906bf83d8dc1ebe4502a0
Vlad <stukalov.blog@gmail.com>
2022-01-10 15:49:15 +0300

    logo

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 import slideMenu from './menu.js';
+import switchHeader from './logoHeader.js';
 
 slideMenu({
     openBtn: '.header__burger-btn',
@@ -6,3 +7,5 @@ slideMenu({
     classActiveMenu: 'navigation_active',
     closeTrigger: '.navigation__link, .navigation__close'
 });
+
+switchHeader();
