acde1be57b32ba00616854b825126d5bf5e4ed48
Vlad <stukalov.blog@gmail.com>
2022-01-12 10:13:39 +0300

    add link videos

--- js/renderCard.js
+++ js/renderCard.js
@@ -1,19 +1,27 @@
+import { getVideo } from "./services.js";
 
 const listCard = document.querySelector('.other-films__list');
 
 
-const renderCard = async (data) => {
+const renderCard = data => {
     listCard.textContent = '';
 
-    const cards = data.map((item)=> {
-                
+    Promise.all(data.map(async (item) => {
+
+        const video = await getVideo(item.id, item.media_type);
+        const key = video.results[0]?.key;
+        console.log('key: ', key);
+
         const card = document.createElement('li');
         card.className = 'other-films__item';
 
         const link = document.createElement('a');
+        if (key) {
+            link.href = `https://youtu.be/${key}`;
+        }
         link.className = 'other-films__link';
-        link.dataset.rating = (item.vote_average || '-');
-        
+        link.dataset.rating = item.vote_average || '-';
+
         const img = document.createElement('img');
         img.className = 'other-films__img';
         img.alt = `постер ${item.title || item.name}`;
@@ -23,9 +31,7 @@ const renderCard = async (data) => {
         card.append(link);
 
         return card;
-    });
-
-    listCard.append(...cards);
+    })).then(cards => listCard.append(...cards));
 };
 
 export default renderCard;
--- js/services.js
+++ js/services.js
@@ -21,7 +21,8 @@ export const getTriends = async (type = 'all', period = 'week', page = 1) => {
 };
 
 export const getTop = async (type, page = 1) => {
-    const url = `${BASE_URL}${type}/top_rated?api_key=${API_KEY}${LANGUAGE}&page=${page}`;
+    const url = new URL(`${type}/top_rated`, BASE_URL);
+    url.search = `api_key=${API_KEY}${LANGUAGE}&page=${page}`;
     return await getData(url);
 };
 
@@ -29,3 +30,17 @@ export const getPopular = async (type, page = 1) => {
     const url = `${BASE_URL}${type}/popular?api_key=${API_KEY}${LANGUAGE}&page=${page}`;
     return await getData(url);
 };
+
+export const getVideo = async (id, type) => {
+    const url = new URL(`${type}/${id}/videos`, BASE_URL);
+    url.search = `api_key=${API_KEY}${LANGUAGE}`;
+    return await getData(url);
+};
+
+export const search = async (query, page) => {
+    const url = new URL(`${type}/${id}/search/multi`, BASE_URL);
+    url.search = `api_key=${API_KEY}${LANGUAGE}&page=${page}&include_adult=false&query=${query}`;
+    return await getData(url);
+};
+
+//https://api.themoviedb.org/3/search/multi?api_key=<<api_key>>&language=en-US&page=1&include_adult=false
